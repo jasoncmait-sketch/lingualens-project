2644ea1ec634e7e84f5b949397d1f681bba5245b
jasonc <jasoncmait@gmail.com>
2025-12-18 03:04:29 +0000

    Create page.tsx
    Create app directory and page file

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+import { ChatInterface } from '../components/ChatInterface'; // Or whatever your main component is named
+
+export default function Home() {
+  return (
+    <main>
+      <ChatInterface />
+    </main>
+  );
+}
